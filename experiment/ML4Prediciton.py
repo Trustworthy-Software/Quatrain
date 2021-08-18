7bcfff44d5116fd324e0bc325c59b357e9a61b8d
haoye tian <363730631@qq.com>
2021-08-18 22:22:54 +0200

    add ML method for predicting

---
+++ experiment/ML4Prediciton.py
@@ -0,0 +1,80 @@
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import KFold, StratifiedKFold
+import numpy as np
+from sklearn.metrics import roc_curve, auc, accuracy_score, recall_score, precision_score
+from sklearn.metrics import confusion_matrix
+
+class Classifier:
+    def __init__(self, dataset, labels, algorithm, kfold):
+        self.dataset = dataset
+        self.labels = labels
+        self.algorithm = algorithm
+        self.kfold = kfold
+
+        print('All data size: {}, Incorrect: {}, Correct: {}'.format(len(self.labels), list(self.labels).count(0), list(self.labels).count(1)))
+        print('Algorithm: {}'.format(self.algorithm))
+        print('---------------')
+    def evaluation_metrics(self, y_true, y_pred_prob):
+        fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_pred_prob, pos_label=1)
+        auc_ = auc(fpr, tpr)
+
+        y_pred = [1 if p >= 0.5 else 0 for p in y_pred_prob]
+        acc = accuracy_score(y_true=y_true, y_pred=y_pred)
+        prc = precision_score(y_true=y_true, y_pred=y_pred)
+        rc = recall_score(y_true=y_true, y_pred=y_pred)
+        f1 = 2 * prc * rc / (prc + rc)
+
+        print('Accuracy: %f -- Precision: %f -- +Recall: %f -- F1: %f ' % (acc, prc, rc, f1))
+        tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+        recall_p = tp / (tp + fn)
+        recall_n = tn / (tn + fp)
+        print('AUC: {:.3f}, +Recall: {:.3f}, -Recall: {:.3f}'.format(auc_, recall_p, recall_n))
+        # return , auc_
+        return auc_, recall_p, recall_n, acc, prc, rc, f1
+
+    def cross_validation(self,):
+        accs, prcs, rcs, f1s, aucs = list(), list(), list(), list(), list()
+        rcs_p, rcs_n = list(), list()
+        skf = StratifiedKFold(n_splits=self.kfold, shuffle=True)
+
+        for train_index, test_index in skf.split(self.dataset, self.labels):
+            x_train, y_train = self.dataset[train_index], self.labels[train_index]
+            x_test, y_test = self.dataset[test_index], self.labels[test_index]
+
+            # standard data
+            scaler = StandardScaler().fit(x_train)
+            # scaler = MinMaxScaler().fit(x_train)
+            x_train = scaler.transform(x_train)
+            x_test = scaler.transform(x_test)
+
+            clf = None
+            if self.algorithm == 'lr':
+                clf = LogisticRegression(solver='lbfgs', max_iter=1000).fit(X=x_train, y=y_train)
+            elif self.algorithm == 'dt':
+                clf = DecisionTreeClassifier().fit(X=x_train, y=y_train, sample_weight=None)
+            elif self.algorithm == 'rf':
+                clf = RandomForestClassifier().fit(X=x_train, y=y_train)
+            elif self.algorithm == 'nb':
+                clf = GaussianNB().fit(X=x_train, y=y_train)
+
+            y_pred = clf.predict_proba(x_test)[:, 1]
+
+            auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=list(y_test), y_pred_prob=list(y_pred))
+
+            accs.append(acc)
+            prcs.append(prc)
+            rcs.append(rc)
+            f1s.append(f1)
+
+            aucs.append(auc_)
+            rcs_p.append(recall_p)
+            rcs_n.append(recall_n)
+
+        print('')
+        print('{}-fold cross validation mean: '.format(self.kfold))
+        print('Accuracy: {:.1f} -- Precision: {:.1f} -- +Recall: {:.1f} -- F1: {:.1f} -- AUC: {:.3f}'.format(np.array(accs).mean()*100, np.array(prcs).mean()*100, np.array(rcs).mean()*100, np.array(f1s).mean()*100, np.array(aucs).mean()))
+        print('AUC: {:.3f}, +Recall: {:.3f}, -Recall: {:.3f}'.format(np.array(aucs).mean(), np.array(rcs_p).mean(), np.array(rcs_n).mean()))
