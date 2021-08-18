f3a1da57ef39c0a96cb647fa35452fb7f307ed42
haoye tian <363730631@qq.com>
2021-08-18 22:51:22 +0200

    add predict

--- experiment/run.py
+++ experiment/run.py
@@ -5,6 +5,8 @@ import pickle
 from scipy.spatial import distance
 from sklearn.metrics import roc_curve, auc, accuracy_score, recall_score, precision_score
 from sklearn.metrics import confusion_matrix, average_precision_score
+import numpy as np
+import ML4Prediciton
 
 class Experiment:
     def __init__(self):
@@ -56,7 +58,7 @@ class Experiment:
         file_name = '../data/bugreport_commit_'+embedding_method+'.pickle'
         if os.path.exists(file_name):
             return
-        dataset = []
+        bugreport_vectors, commit_vectors = [], []
         with open('../data/bugreport_dict_'+embedding_method+'.pickle', 'rb') as f:
             self.bugreport_closure = pickle.load(f)
         for root, dirs, files in os.walk(path_patch):
@@ -80,8 +82,10 @@ class Experiment:
                         commit = f.readlines()[0]
                     w = Word2vector(embedding_method)
                     commit_vector = w.embedding(commit)
-                    sample = [bug_report, commit_vector, label]
-                    dataset.append(sample)
+
+                    bugreport_vectors.append(bug_report)
+                    commit_vectors.append(commit_vector)
+                    labels.append(label)
 
                     # dist = distance.euclidean(bug_report, commit_vector)/(1+distance.euclidean(bug_report, commit_vector))
                     # similarity = 1- dist
@@ -93,11 +97,24 @@ class Experiment:
                     # labels.append(label)
                     # y_preds.append(y_pred)
 
-        pickle.dump(dataset, open('../data/bugreport_commit_'+embedding_method+'.pickle', 'wb'))
+        pickle.dump([bugreport_vectors, commit_vectors, labels], open('../data/bugreport_commit_'+embedding_method+'.pickle', 'wb'))
         # self.evaluation_metrics(labels, y_preds)
+
+    def predict(self, embedding_method):
+        dataset = pickle.load(open('../data/bugreport_commit_'+embedding_method+'.pickle', 'rb'))
+        bugreport_vector = np.array(dataset[0]).reshape((len(dataset[0]),-1))
+        commit_vector = np.array(dataset[1]).reshape((len(dataset[1]),-1))
+        labels = np.array(dataset[2])
+
+        # combine bug report and commit message of patch
+        features = np.concatenate((bugreport_vector, commit_vector), axis=1)
+        cl = ML4Prediciton.Classifier(features, labels, 'rf', 10)
+        cl.cross_validation()
+
 
 if __name__ == '__main__':
     embedding = 'bert'
     e = Experiment()
     e.save_bugreport(project='Closure', embedding_method=embedding)
     e.save_commit(e.cf.path_patch, embedding_method=embedding)
+    e.predict(embedding)
