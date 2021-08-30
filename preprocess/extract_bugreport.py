23188c222e1e5781c8fd7ccba41507cf4c8ac89d
haoye tian <363730631@qq.com>
2021-08-30 16:30:11 +0200

    add Chart and Time bug report extraction

--- preprocess/extract_bugreport.py
+++ preprocess/extract_bugreport.py
@@ -9,13 +9,13 @@ def get_json_by_url(folder_path, names, lists):
         os.makedirs(folder_path)
     for i in range(len(lists)):
         url = lists[i]
-        print("Try downloading file: {}".format(url))
         filename = names[i] + '.json'
         filepath = folder_path + '/' + filename
         if os.path.exists(filepath):
             print("File have already exist. skip")
         else:
             try:
+                print("Try downloading file: {}".format(url))
                 urllib.urlretrieve(url, filename=filepath)
             except Exception as e:
                 print("Error occurred when downloading file, error message:")
@@ -39,7 +39,7 @@ def get_json(file_json):
 def save_bug_report(path_folder):
     result = ''
     json_list = os.listdir(path_folder)
-    json_list.sort()
+    json_list = sorted(json_list, key=lambda x: int(x.split('.')[0].split('-')[1]),)
     for j in json_list:
         with open(os.path.join(path_folder,j), 'r+') as f:
             name = j.split('.')[0]
@@ -60,6 +60,7 @@ def get_bug_report_closure(file_json):
 def get_bug_report(project):
     path_xml = 'Project_File/' + project + '_xml'
     xml_names = os.listdir(path_xml)
+    xml_names = sorted(xml_names, key=lambda x: int(x.split('.')[0].split('-')[1]),)
     result = ''
     for xml_name in xml_names:
         path_file = path_xml + '/' + xml_name
@@ -69,6 +70,10 @@ def get_bug_report(project):
             the_page = pickle.load(f)
         soup = bs.BeautifulSoup(the_page, "html.parser")
 
+        # if project == 'Time':
+        #     summary = soup.find('span', {'data-snek-id': 'issue-title'}).text.strip().replace('\n',' ')
+        #     description = 'None'
+        # else:
         summary = soup.find('h1', {'id': 'summary-val'}).text.strip().replace('\n',' ')
         description = soup.find('div', {'id': 'description-val'}).text.strip().replace('\n',' ')
         result += project_id + '$$' + summary + '$$' + description + '\n'
@@ -76,15 +81,36 @@ def get_bug_report(project):
     with open('Project_BugReport/'+project+'_bugreport.txt', 'w+') as f:
         f.write(result)
 
+def get_bug_report_chartTime(project):
+    path_json = 'Project_File/' + project + '_json'
+    json_names = os.listdir(path_json)
+    json_names = sorted(json_names, key=lambda x: int(x.split('.')[0].split('-')[1]),)
+    result = ''
+    for json_name in json_names:
+        path_file = path_json + '/' + json_name
+        project_id = json_name.split('.')[0]
+        with open(path_file, 'r+') as f:
+            dict = json.load(f)
+            summary = dict['summary'].strip()
+            description = dict['description'].strip().replace('\n',' ')
+        result += project_id + '$$' + summary + '$$' + description + '\n'
+    print (result)
+    with open('Project_BugReport/'+project+'_bugreport.txt', 'w+') as f:
+        f.write(result)
+
 if __name__ == '__main__':
-    # extract bug report of Closure
-    file_json = 'Project_URL/Closure_url.txt'
-    get_bug_report_closure(file_json)
+    # '''
+    # 1. extract bug report of Closure
+    get_bug_report_closure(file_json='Project_URL/Closure_url.txt')
 
-    # extract bug report of  Lang
-    project = 'Lang'
-    get_bug_report(project)
+    # 2. extract bug report of  Lang
+    get_bug_report(project='Lang')
 
-    # extract bug report of Math
-    project = 'Math'
-    get_bug_report(project)
+    # 3. extract bug report of Math
+    get_bug_report(project='Math')
+
+    # 4. extract bug report of Chart
+    get_bug_report_chartTime(project='Chart')
+    # '''
+    # 3. extract bug report of Math
+    get_bug_report_chartTime(project='Time')
