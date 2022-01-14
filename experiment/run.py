f9df7f3bca3816a36b57dba3cdfaad18ce882e51
haoye tian <363730631@qq.com>
2022-01-14 17:15:03 +0100

    move file

--- experiment/run.py
+++ experiment/run.py
@@ -14,7 +14,7 @@ class Experiment:
     def __init__(self):
         self.cf = config.Config()
         self.path_patch = self.cf.path_patch
-        if 'Unique' in self.path_patch:
+        if not 'Unique' in self.path_patch:
             raise ('please deduplicate it!')
         self.path_patch_sliced = self.cf.path_patch + '_sliced'
         # self.dict_b = {}
@@ -78,102 +78,9 @@ class Experiment:
                     dict_b[project_id] = [bugReportSummary, bugReportDescription]
         pickle.dump(dict_b, open(file_name, 'wb'))
 
-    def save_bugreport_patch(self, path_patch, ):
-        # dataset_text = ''
-        dataset_text_with_description = ''
-        file_name = '../data/bugreport_patch.txt'
-        if os.path.exists(file_name):
-            return
-        with open('../data/bugreport_dict.pickle', 'rb') as f:
-            self.bugReportText = pickle.load(f)
-        for root, dirs, files in os.walk(path_patch):
-            for file in files:
-                if file.endswith('.patch'):
-                    name = file.split('.')[0]
-                    label = 1 if root.split('/')[-4] == 'Correct' else 0
-                    project = name.split('-')[1]
-                    # if project != 'Closure':
-                    #     continue
-                    id = name.split('-')[2]
-                    project_id = project + '-' + id
-                    print('collecting {}'.format(project_id))
-                    commit_file = name + '.txt'
-                    try:
-                        with open(os.path.join(root, commit_file), 'r+') as f:
-                            commit_content = f.readlines()[0].strip('\n')
-                    except Exception as e:
-                        print(e)
-                        commit_content = ''
-                    if project_id in self.bugReportText.keys():
-                        bug_report_text = self.bugReportText[project_id]
-                        bug_report_summary = bug_report_text[0].strip()
-                        bug_report_description = bug_report_text[1].strip()
-                    else:
-                        bug_report_summary = 'None'
-                        bug_report_description = 'None'
-                    # TODO: use description info of bug report
-                    # dataset_text += '$$'.join([project_id, bug_report_summary, name, commit_content, str(label)]) + '\n'
-                    dataset_text_with_description += '$$'.join([project_id, bug_report_summary, bug_report_description, name, commit_content, str(label)]) + '\n'
-        with open(file_name, 'w+') as f:
-            f.write(dataset_text_with_description)
 
 
-    def save_bugreport_patch_vector(self, embedding_method):
-        labels, y_preds = [], []
-        deduplicated_patchids = pickle.load(open('../utils/deduplicated_name.pickle', 'rb'))
-        file_name = '../data/bugreport_patch_'+embedding_method+'.pickle'
-        if os.path.exists(file_name):
-            return
-        bugreport_vectors, commit_vectors = [], []
-        bugreport_v2_vectors = []
-        cnt = 0
-        signal.signal(signal.SIGALRM, self.handler)
-        with open('../data/bugreport_patch.txt', 'r+') as f:
-            for line in f:
-                    project_id = line.split('$$')[0].strip()
-                    bugreport_summary = line.split('$$')[1].strip()
-                    bugreport_description = line.split('$$')[2].strip()
-                    patch_id = line.split('$$')[3].strip()
-                    commit_content = line.split('$$')[4].strip()
-                    label = int(float(line.split('$$')[5].strip()))
-                    # skip none and duplicated cases
-                    if bugreport_summary == 'None' or commit_content == 'None' or (patch_id not in deduplicated_patchids):
-                        continue
-                    w = Word2vector(embedding_method)
-
-                    signal.alarm(300)
-                    try:
-                        bugreport_vector = w.embedding(bugreport_summary)
-                        bugreport_v2_vector = w.embedding(bugreport_summary+'.'+bugreport_description)
-                        commit_vector = w.embedding(commit_content)
-                    except Exception as e:
-                        print(e)
-                        continue
-                    signal.alarm(0)
-
-                    bugreport_vectors.append(bugreport_vector)
-                    bugreport_v2_vectors.append(bugreport_v2_vector)
-                    commit_vectors.append(commit_vector)
-                    labels.append(label)
-
-                    cnt += 1
-                    print('{} {}'.format(cnt, project_id))
-                    # dist = distance.euclidean(bug_report, commit_vector)/(1+distance.euclidean(bug_report, commit_vector))
-                    # similarity = 1- dist
-                    # if similarity >= 0.5:
-                    #     y_pred = 1
-                    # else:
-                    #     y_pred = 0
-                    #
-                    # labels.append(label)
-                    # y_preds.append(y_pred)
-
-
-        pickle.dump([bugreport_vectors, commit_vectors, labels], open('../data/bugreport_patch_'+embedding_method+'.pickle', 'wb'))
-        pickle.dump([bugreport_v2_vectors, commit_vectors, labels], open('../data/bugreport_patch_'+embedding_method+'(description).pickle', 'wb'))
-        # self.evaluation_metrics(labels, y_preds)
-
-    def predict(self, embedding_method):
+    def predict_10fold(self, embedding_method):
         dataset = pickle.load(open('../data/bugreport_patch_'+embedding_method+'.pickle', 'rb'))
         bugreport_vector = np.array(dataset[0]).reshape((len(dataset[0]),-1))
         commit_vector = np.array(dataset[1]).reshape((len(dataset[1]),-1))
@@ -245,18 +152,12 @@ class Experiment:
         cl.cross_validation()
 
 
-    def handler(signum, frame):
-        raise Exception("end of time")
+
 
 if __name__ == '__main__':
     embedding = 'bert'
     e = Experiment()
 
-    e.save_bugreport()
-    e.save_bugreport_patch(e.path_patch,)
-    e.save_bugreport_patch_vector(embedding_method=embedding)
-
-    # e.predict(embedding)
-    e.predict(embedding+'(description)')
-
-    e.predictASE(e.path_patch_sliced)
+    e.predict_10fold(embedding)
+    e.predict_10fold(embedding+'(description)')
+    # e.predictASE(e.path_patch_sliced)
---
+++ experiment/save_bugreport_patch.py
@@ -0,0 +1,125 @@
+import config
+import os
+from representation.word2vec import Word2vector
+import pickle
+from scipy.spatial import distance
+from sklearn.metrics import roc_curve, auc, accuracy_score, recall_score, precision_score
+from sklearn.metrics import confusion_matrix, average_precision_score
+import numpy as np
+import ML4Prediciton
+import signal
+import json
+
+def save_bugreport_patch(path_patch, ):
+    # dataset_text = ''
+    dataset_text_with_description = ''
+    file_name = '../data/bugreport_patch.txt'
+    if os.path.exists(file_name):
+        return
+    with open('../data/BugReport/Bug_Report_All.json', 'rb') as f:
+        bugReportText = json.load(f)
+
+    cnt_patch, cnt_patch_with_bugreport = 0, 0
+    datasets = os.listdir(path_patch)
+    for dataset in datasets:
+        path_dataset = os.path.join(path_patch, dataset)
+        benchmarks = os.listdir(path_dataset)
+        for benchmark in benchmarks:
+            path_benchmark = os.path.join(path_dataset, benchmark)
+            tools = os.listdir(path_benchmark)
+            for tool in tools:
+                path_tool = os.path.join(path_benchmark, tool)
+                labels = os.listdir(path_tool)
+                for label in labels:
+                    path_label = os.path.join(path_tool, label)
+                    projects = os.listdir(path_label)
+                    for project in projects:
+                        path_project = os.path.join(path_label, project)
+                        ids = os.listdir(path_project)
+                        for id in ids:
+                            path_id = os.path.join(path_project, id)
+                            patches = os.listdir(path_id)
+                            for patch in patches:
+                                cnt_patch += 1
+                                if benchmark == 'Bugsjar':
+                                    if '+' in project:
+                                        project = project.split('+')[1]
+                                        project = project.upper()
+                                        project_id = project + '-' + id
+                                else:
+                                    project_id = project + '-' + id
+                                print('collecting {}'.format(project_id))
+                                label_int = 1 if label == 'Correct' else 0
+
+                                # extract bug report
+                                if project_id in bugReportText.keys():
+                                    bug_report_text = bugReportText[project_id]
+                                    bug_report_summary = bug_report_text[0].strip()
+                                    bug_report_description = bug_report_text[1].strip()
+                                else:
+                                    bug_report_summary = 'None'
+                                    bug_report_description = 'None'
+
+                                # extract patch text
+                                patch_text = ''
+                                path_single_patch = os.path.join(path_id, patch)
+                                for root, dirs, files in os.walk(path_single_patch):
+                                    for file in files:
+                                        if file.endswith('.txt'):
+                                            patch_id = file.split('.')[0]
+                                            try:
+                                                with open(os.path.join(root, file), 'r+') as f:
+                                                    patch_text += f.readlines()[0].strip('\n')
+                                            except Exception as e:
+                                                print(e)
+                                                continue
+
+                                if bug_report_summary == 'None' or patch_text == '':
+                                    continue
+                                # integrate and save them
+                                # TODO: use test suite exception
+                                # dataset_text += '$$'.join([project_id, bug_report_summary, name, commit_content, str(label)]) + '\n'
+                                dataset_text_with_description += '$$'.join(
+                                    [project_id, bug_report_summary, bug_report_description, patch_id, patch_text,
+                                     str(label_int)]) + '\n'
+                                cnt_patch_with_bugreport += 1
+    with open(file_name, 'w+') as f:
+        f.write(dataset_text_with_description)
+    print('patch number: {}, patch with available bug report: {}'.format(cnt_patch, cnt_patch_with_bugreport))
+
+    # for root, dirs, files in os.walk(path_patch):
+    #     for file in files:
+    #         if file.endswith('.patch'):
+    #             name = file.split('.')[0]
+    #             label = 1 if root.split('/')[-4] == 'Correct' else 0
+    #             project = name.split('-')[1]
+    #             # if project != 'Closure':
+    #             #     continue
+    #             id = name.split('-')[2]
+    #             project_id = project + '-' + id
+    #             print('collecting {}'.format(project_id))
+    #             commit_file = name + '.txt'
+    #             try:
+    #                 with open(os.path.join(root, commit_file), 'r+') as f:
+    #                     commit_content = f.readlines()[0].strip('\n')
+    #             except Exception as e:
+    #                 print(e)
+    #                 commit_content = ''
+    #             if project_id in self.bugReportText.keys():
+    #                 bug_report_text = self.bugReportText[project_id]
+    #                 bug_report_summary = bug_report_text[0].strip()
+    #                 bug_report_description = bug_report_text[1].strip()
+    #             else:
+    #                 bug_report_summary = 'None'
+    #                 bug_report_description = 'None'
+    #             # TODO: use description info of bug report
+    #             # dataset_text += '$$'.join([project_id, bug_report_summary, name, commit_content, str(label)]) + '\n'
+    #             dataset_text_with_description += '$$'.join([project_id, bug_report_summary, bug_report_description, name, commit_content, str(label)]) + '\n'
+    # with open(file_name, 'w+') as f:
+    #     f.write(dataset_text_with_description)
+
+if __name__ == '__main__':
+    path_patch = config.Config().path_patch
+
+    # save bug report and patch in text
+    save_bugreport_patch(path_patch)
