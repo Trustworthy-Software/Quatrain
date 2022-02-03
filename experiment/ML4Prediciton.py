45147d33eee5eba166428c02f70926cce4290c47
haoye tian <363730631@qq.com>
2022-02-03 17:03:22 +0100

    add leave1out

--- experiment/ML4Prediciton.py
+++ experiment/ML4Prediciton.py
@@ -10,20 +10,22 @@ from sklearn.metrics import confusion_matrix
 import xgboost as xgb
 
 class Classifier:
-    def __init__(self, dataset, labels, algorithm, kfold):
+    def __init__(self, dataset, labels, algorithm, kfold, train_features=None, train_labels=None, test_features=None, test_labels=None):
         self.dataset = dataset
         self.labels = labels
         self.algorithm = algorithm
         self.kfold = kfold
 
-        print('All data size: {}, Incorrect: {}, Correct: {}'.format(len(self.labels), list(self.labels).count(0), list(self.labels).count(1)))
-        print('Algorithm: {}'.format(self.algorithm))
-        print('#####')
+        self.train_features = train_features
+        self.train_labels = train_labels
+        self.test_features = test_features
+        self.test_labels = test_labels
+
     def evaluation_metrics(self, y_true, y_pred_prob):
         fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_pred_prob, pos_label=1)
         auc_ = auc(fpr, tpr)
 
-        y_pred = [1 if p >= 0.5 else 0 for p in y_pred_prob]
+        y_pred = [1 if p >= 0.50 else 0 for p in y_pred_prob]
         acc = accuracy_score(y_true=y_true, y_pred=y_pred)
         prc = precision_score(y_true=y_true, y_pred=y_pred)
         rc = recall_score(y_true=y_true, y_pred=y_pred)
@@ -38,6 +40,10 @@ class Classifier:
         return auc_, recall_p, recall_n, acc, prc, rc, f1
 
     def cross_validation(self,):
+        print('All data size: {}, Incorrect: {}, Correct: {}'.format(len(self.labels), list(self.labels).count(0), list(self.labels).count(1)))
+        print('Algorithm: {}'.format(self.algorithm))
+        print('#####')
+
         accs, prcs, rcs, f1s, aucs = list(), list(), list(), list(), list()
         rcs_p, rcs_n = list(), list()
         skf = StratifiedKFold(n_splits=self.kfold, shuffle=True)
@@ -88,3 +94,38 @@ class Classifier:
         print('Accuracy: {:.1f} -- Precision: {:.1f} -- +Recall: {:.1f} -- F1: {:.1f} -- AUC: {:.3f}'.format(np.array(accs).mean()*100, np.array(prcs).mean()*100, np.array(rcs).mean()*100, np.array(f1s).mean()*100, np.array(aucs).mean()))
         print('AUC: {:.3f}, +Recall: {:.3f}, -Recall: {:.3f}'.format(np.array(aucs).mean(), np.array(rcs_p).mean(), np.array(rcs_n).mean()))
         print('---------------')
+
+    def leave1out_validation(self):
+        x_train, y_train = self.train_features, self.train_labels
+        x_test, y_test = self.test_features, self.test_labels
+
+        # standard data
+        scaler = StandardScaler().fit(x_train)
+        # scaler = MinMaxScaler().fit(x_train)
+        x_train = scaler.transform(x_train)
+        x_test = scaler.transform(x_test)
+
+        clf = None
+        if self.algorithm == 'lr':
+            clf = LogisticRegression(solver='lbfgs', max_iter=1000).fit(X=x_train, y=y_train)
+        elif self.algorithm == 'dt':
+            clf = DecisionTreeClassifier().fit(X=x_train, y=y_train, sample_weight=None)
+        elif self.algorithm == 'rf':
+            clf = RandomForestClassifier().fit(X=x_train, y=y_train, )
+        elif self.algorithm == 'xgb':
+            dtrain = xgb.DMatrix(x_train, label=y_train)
+            clf = xgb.train(params={'objective': 'binary:logistic', 'verbosity': 0}, dtrain=dtrain, num_boost_round=500)
+        elif self.algorithm == 'nb':
+            clf = GaussianNB().fit(X=x_train, y=y_train)
+
+        if self.algorithm == 'xgb':
+            x_test_xgb = x_test
+            x_test_xgb_dmatrix = xgb.DMatrix(x_test_xgb, label=y_test)
+            y_pred = clf.predict(x_test_xgb_dmatrix)
+        else:
+            y_pred = clf.predict_proba(x_test)[:, 1]
+
+        auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=list(y_test),
+                                                                             y_pred_prob=list(y_pred))
+        print('---------------')
+        return auc_, recall_p, recall_n, acc, prc, rc, f1
