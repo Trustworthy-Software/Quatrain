a194cb48c26e623df57721ed5449fc59b89d67a5
haoye tian <363730631@qq.com>
2022-02-03 11:20:36 +0100

    add bug report patch txt

---
+++ data/BugReport/assemble_bug_report.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+path = '../data/BugReport'
+
+def assemble(path):
+    bug_reprot_dict = {}
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.txt'):
+                with open(os.path.join(root, file), 'r+') as f:
+                    for line in f:
+                        bugid = line.split('$$')[0]
+                        summary = line.split('$$')[1]
+                        description = line.split('$$')[2]
+                        bug_reprot_dict[bugid] = [summary, description]
+
+    with open('../data/BugReport/Bug_Report_All.json', 'w+') as f2:
+        json.dump(bug_reprot_dict, f2)
+
+
+if __name__ == '__main__':
+    assemble(path)
