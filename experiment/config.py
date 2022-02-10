aa881fef42ae917f51338d38e9c4f268168ecb98
haoye tian <363730631@qq.com>
2022-02-10 14:28:59 +0100

    change path

--- experiment/config.py
+++ experiment/config.py
@@ -1,4 +1,5 @@
 
 class Config:
     def __init__(self):
-        self.path_patch = '/Users/haoye.tian/Documents/University/data/PatchCollectingV1Natural'
+        # self.path_patch = '/Users/haoye.tian/Documents/University/data/PatchCollectingV1Natural'
+        self.path_patch = '/Users/haoye.tian/Documents/ISSTA2022withTextUnique'
