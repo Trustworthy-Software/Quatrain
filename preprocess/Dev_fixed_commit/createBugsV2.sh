551f2113b00d2e29210defe21253c23fcee45844
haoyetiancoder <363730631@qq.com>
2022-02-28 22:46:19 +0100

    get developer/official commit message

---
+++ preprocess/Dev_fixed_commit/createBugsV2.sh
@@ -0,0 +1,66 @@
+dir=/Users/haoye.tian/Documents/University/project/defects4j_buggy/
+export PATH=$PATH:/Users/haoye.tian/Documents/University/project/defects4j/framework/bin
+pwd=/Users/haoye.tian/Documents/University/project/Naturality/preprocess/Dev_fixed_commit/
+
+proj=Chart
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+proj=Cli
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+proj=Closure
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=Codec
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+proj=Collections
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=Compress
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=Csv
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+proj=Gson
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=JacksonCore
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=JacksonDatabind
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=JacksonXml
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=Jsoup
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=JxPath
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=Lang
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=Math
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=Mockito
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
+
+
+proj=Time
+defects4j query -p $proj -q "revision.id.fixed" > $pwd$proj".txt"
