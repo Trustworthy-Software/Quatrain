4697d7d7de4c4eade55bc38c57d002d14867e3f5
Haoye Tian <haoye@trux01.uni.lu>
2022-03-27 17:03:34 +0200

    fix path

--- experiment/config.py
+++ experiment/config.py
@@ -2,4 +2,4 @@
 class Config:
     def __init__(self):
         # self.path_patch = '/Users/haoye.tian/Documents/University/data/PatchCollectingV1Natural'
-        self.path_patch = '../../ISSTA2022withTextUnique'
+        self.path_patch = '../ISSTA2022withTextUnique'
