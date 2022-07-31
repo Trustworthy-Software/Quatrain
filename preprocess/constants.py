50fa8aec93f87bef5e27db32ac6f19fff0d14a2f
haoyetiancoder <363730631@qq.com>
2022-07-31 17:50:56 +0200

    rm file

--- preprocess/constants.py
+++
@@ -1,9 +0,0 @@
-PAD_CHAR = "**PAD**"
-EMBEDDING_SIZE = 100
-MAX_LENGTH = 2500
-
-#where you want to save any models you may train
-MODEL_DIR = '../saved_models/'
-
-Origin_DATA_DIR = '../data/'
-Output_DATA_DIR = '../data/'
