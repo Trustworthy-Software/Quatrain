8fc0c83d1359024e446737b1bfd9a0edff5f9fea
haoyetiancoder <363730631@qq.com>
2022-07-31 21:43:41 +0200

    update

--- experiment/config.py
+++ experiment/config.py
@@ -1,4 +1,7 @@
 
 class Config:
     def __init__(self):
+        # original dataset with patches text and commit messages text.
         self.path_patch = '/Users/haoye.tian/Documents/ASE2022withTextUnique'
+        # ASE2020 feature for RQ3 DL
+        self.path_ASE2020_feature = '/Users/haoye.tian/Documents/University/data/ASE_features2_bert.pickle'
