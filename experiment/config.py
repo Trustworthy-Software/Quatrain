22a41da68fd64fdf8285a7014d7d485345705f10
haoyetiancoder <363730631@qq.com>
2022-08-03 23:32:20 +0200

    comments

--- experiment/config.py
+++ experiment/config.py
@@ -11,5 +11,5 @@ class Config:
 
         # original dataset with patches text and commit messages text.
         self.path_patch = '/Users/haoye.tian/Documents/ASE2022withTextUnique'
-        # Tian et al.'s ASE2020 feature for RQ3 DL experiment.
+        # the feature from Tian et al.'s ASE2020 paper for RQ3 DL experiment.
         self.path_ASE2020_feature = '/Users/haoye.tian/Documents/University/data/ASE_features2_bert.pickle'
